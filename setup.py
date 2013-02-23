8dc960def76953754fb18b7eefe87a043bf08a4d
Mauro Bianchi <bianchimro@gmail.com>
2013-02-23 12:43:21 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='genepi',
+      version='0.1',
+      description='Genetici algorithms in python',
+      url='http://github.com/bianchimro/genepi',
+      author='Mauro Bianchi',
+      author_email='bianchimro@gmail.com',
+      license='MIT',
+      packages=['genepi'],
+      zip_safe=False)
