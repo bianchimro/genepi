898548b91a85bd3551578f0027b6232233e34baa
Mauro Bianchi <bianchimro@gmail.com>
2013-02-23 12:54:12 +0100

    basic package

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='genepi',
       version='0.1',
       description='Genetici algorithms in python',
+      long_description=readme(),
       url='http://github.com/bianchimro/genepi',
       author='Mauro Bianchi',
       author_email='bianchimro@gmail.com',
       license='MIT',
       packages=['genepi'],
+      install_requires=[],
+      test_suite='nose.collector',
+      tests_require=['nose'],
       zip_safe=False)
